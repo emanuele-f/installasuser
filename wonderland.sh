e7a87bd3009edefe0dabcf09c6effdc51343d353
emanuele-f <black.silver@hotmail.it>
2015-02-27 19:23:23 +0100

    Custom proot bindings and use copy on write

--- wonderland.sh
+++ wonderland.sh
@@ -11,6 +11,34 @@ UNIONDIR="./rootfs"
 PKGCACHE="./pkgcache"
 PKGDIRNAME="debget"
 
+function usage()
+{
+	echo "Usage: `basename $0` [options]";
+	echo "Options:";
+	echo -e " -h\t\tshow this help";
+	echo -e " -b [res] [pat]\tadd proot bind option: system [res] available at [pat]";
+	exit 1
+}
+
+function add_proot_bind()
+{
+    # $1 : resource relative path
+    # $2 : bind point | null -> same path
+
+    res="`readlink -f \"$1\"`"
+    if [ ! -z "$2" ]; then
+        bpoint="$2"
+    else
+        bpoint="$res"
+    fi
+
+    if [ ! -e "$res" ]; then
+        echo "Resource '$res' not found"
+        exit 1
+    fi
+    BINDINGS="$BINDINGS -b $res:$bpoint"
+}
+
 function util_lookup()
 {
     # looks for $1 command and tries to download it
@@ -26,6 +54,23 @@ function util_lookup()
     fi
 
 }
+
+# parse parameters
+while [ ! -z "$1" ]; do
+	case "$1" in
+	-h) usage
+		;;
+	-b) [ -z "$2" -o -z "$3" ] && usage
+		res="$2"
+		to="$3"
+        add_proot_bind "$2" "$3"
+		shift
+		shift
+		;;
+	*) 	usage
+	esac
+	shift
+done
 
 # make absolute
 UNIONDIR="`readlink -f \"$UNIONDIR\"`"
@@ -58,23 +103,26 @@ if [ $? -ne 0 ]; then
     chmod +x "$ROOTDIR/usr/bin/debget"
 fi
 
+# System bindings - their modifications will affect actual system filesystem
+add_proot_bind /dev
+add_proot_bind /tmp
+add_proot_bind /proc
+add_proot_bind /sys
+add_proot_bind /run
+
+# Additional bound points
+add_proot_bind "$ROOTDIR" /sysroot
+add_proot_bind "$PKGCACHE" /debget/_pool
+
 # Mount the union
 grep "$UNIONDIR" /etc/mtab >/dev/null
 [ $? -eq 0 ] && fusermount -u "$UNIONDIR"
-"$CMD_UNIONFS" "$ROOTDIR"=RW:/=RO "$UNIONDIR"
+"$CMD_UNIONFS" -o cow "$ROOTDIR"=RW:/=RO "$UNIONDIR"
 [ $? -ne 0 ] && exit 1
 
-# make user-changes-directory available at /sysroot
-# make package cache available at /debget/_pool
+# Enter the jail
 cd /
-"$CMD_PROOT" -r "$UNIONDIR"\
-    -b "$ROOTDIR":/sysroot\
-    -b "$PKGCACHE":/debget/_pool\
-    -b "/dev":/dev\
-    -b "/sys":/sys\
-    -b "/tmp":/tmp\
-    -b "/proc":/proc\
-    /bin/bash
+"$CMD_PROOT" -r "$UNIONDIR" $BINDINGS /bin/bash
 
 # Umount
 fusermount -u "$UNIONDIR"
